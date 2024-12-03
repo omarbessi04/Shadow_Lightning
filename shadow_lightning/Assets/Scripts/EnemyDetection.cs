99db49f4158fa2f7ada7d24a36f1897639c69a8c
tryggvi04 <tryggvi04@gmail.com>
2024-12-03 13:02:07 +0000

    movement

---
+++ shadow_lightning/Assets/Scripts/EnemyDetection.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EnemyDetection : MonoBehaviour
+{
+   void OnTriggerEnter2D(Collider2D other)
+   {
+      if (other.tag == "PlayerEnemy")
+      {
+         print("Detected");
+      }
+   }
+}
