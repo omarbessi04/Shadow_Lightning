8bfe234b26fc3a9130d8a8c408a024cd633f614f
Ómar Bessi Ómarsson <omarbessi04@gmail.com>
2024-12-05 13:53:56 +0000

    Finish Pillar Animation + complete scene transition

--- shadow_lightning/Assets/Scripts/PillarAnimHandler.cs
+++ shadow_lightning/Assets/Scripts/PillarAnimHandler.cs
@@ -1,14 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PillarAnimHandler : MonoBehaviour
 {
     public Animator my_animator;
+    public float moveSpeed = 1f;
+    public float targetHeight = 5f;
+    private bool isMovingUp = false;
+    private float initialY;
 
-    private void Update() {
-        if (GameManager.instance.alive_enemy_count == 0){
+    private void Start()
+    {
+        initialY = transform.position.y;
+    }
+
+    private void Update()
+    {
+        if (GameManager.instance.alive_enemy_count == 0)
+        {
             my_animator.SetBool("RoomCleared", true);
+            MoveUp();
         }
+
+        if (isMovingUp)
+        {
+            float newY = Mathf.MoveTowards(transform.position.y, initialY + targetHeight, moveSpeed * Time.deltaTime);
+            transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+
+            if (Mathf.Approximately(transform.position.y, initialY + targetHeight))
+            {
+                isMovingUp = false;
+            }
+        }
+    }
+
+    public void MoveUp()
+    {
+        isMovingUp = true;
     }
 }
