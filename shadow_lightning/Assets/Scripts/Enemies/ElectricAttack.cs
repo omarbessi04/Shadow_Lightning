59551bac74eb103cb10d815460c3c26462971c26
tryggvi04 <tryggvi04@gmail.com>
2024-12-12 14:57:51 +0000

    shield bugfix

--- shadow_lightning/Assets/Scripts/Enemies/ElectricAttack.cs
+++ shadow_lightning/Assets/Scripts/Enemies/ElectricAttack.cs
@@ -9,31 +9,62 @@ public class ElectricAttack : MonoBehaviour
 
     public Vector2 direction;
     public float Damage = 0.5f;
+    private Collider2D collider;
+    private GameObject player;
 
     public float speed = 5f;
+
+    private bool behind;
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("PlayerEnemy");
         Destroy(gameObject,10f);
+        collider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (direction == Vector2.right &&(player.transform.position.x - transform.position.x) < 0)
+        {
+            behind = true;
+        }
+        else if (direction == Vector2.left && (player.transform.position.x - transform.position.x) > 0)
+        {
+            behind = true;
+        }
+        else
+        {
+            behind = false;
+        }
        transform.Translate(direction*speed*Time.deltaTime); 
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "PlayerEnemy")
+        if (other.GameObject().name == "GroundTiles")
         {
+            Destroy(gameObject);
+        }
+        if (other.tag == "PlayerEnemy"){
+        
             if (other.GameObject().GetComponent<PlayerMovement>().type == "Shield")
             {
-                if (direction == Vector2.right && !other.GameObject().GetComponentInChildren<PlayerAnimator>().lookingRight)
+                if (direction == Vector2.right && !other.GameObject().GetComponentInChildren<PlayerAnimator>().lookingRight && !behind)
                 {
                     Destroy(gameObject);
                 }
-                else if (direction == Vector2.left && other.GameObject().GetComponentInChildren<PlayerAnimator>().lookingRight)
+                else if (direction == Vector2.left && other.GameObject().GetComponentInChildren<PlayerAnimator>().lookingRight && !behind)
+                {
+                    Destroy(gameObject);
+                }
+                else if (direction == Vector2.left &&
+                         !other.GameObject().GetComponentInChildren<PlayerAnimator>().lookingRight && behind)
+                {
+                    Destroy(gameObject);
+                }
+                else if (direction == Vector2.right && other.GameObject().GetComponentInChildren<PlayerAnimator>().lookingRight && behind)
                 {
                     Destroy(gameObject);
                 }
