2023b4bb430ee32237dbe5a337adf54bda84c97a
TristanOrri <142910326+TristanOrri@users.noreply.github.com>
2024-12-14 20:32:40 +0000

    Credits scene and button to ge tthere and back are setup

--- shadow_lightning/Assets/Scripts/Menu/MainMenuManager.cs
+++ shadow_lightning/Assets/Scripts/Menu/MainMenuManager.cs
@@ -4,10 +4,24 @@ using UnityEngine.SceneManagement;
 public class MainMenuManager : MonoBehaviour
 {
     [SerializeField] private string gameScreen;
+    [SerializeField] private string creditsScreen;
+    [SerializeField] private string menuScreen;
     public void PlayGame()
     {
         SceneManager.LoadScene(gameScreen);
         GameManager.instance.currentScene = gameScreen;
+    }
+
+    public void GoToCredits()
+    {
+        SceneManager.LoadScene(creditsScreen);
+        GameManager.instance.currentScene = creditsScreen;
+    }
+
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene(menuScreen);
+        GameManager.instance.currentScene = menuScreen;
     }
 
     public void QuitGame()
